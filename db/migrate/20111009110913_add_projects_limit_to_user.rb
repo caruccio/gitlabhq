ccf76c4a6507c2cd633b4360c3603bc5d1a59003
Mateus Caruccio <mateus.caruccio@corp.terra.com.br>
2012-12-19 21:18:04 -0200

    limit 1 project pre user

--- db/migrate/20111009110913_add_projects_limit_to_user.rb
+++ db/migrate/20111009110913_add_projects_limit_to_user.rb
@@ -1,5 +1,5 @@
 class AddProjectsLimitToUser < ActiveRecord::Migration
   def change
-    add_column :users, :projects_limit, :integer, :default => 10
+    add_column :users, :projects_limit, :integer, :default => 1
   end
 end
