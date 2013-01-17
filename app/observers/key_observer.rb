972193e9f9a6e5b31c30a658785492a44ab903af
Mateus Caruccio <mateus.caruccio@corp.terra.com.br>
2013-01-17 10:10:39 -0200

    added key hooks

--- app/observers/key_observer.rb
+++ app/observers/key_observer.rb
@@ -3,10 +3,28 @@ class KeyObserver < ActiveRecord::Observer
 
   def after_save(key)
     git_host.set_key(key.identifier, key.key, key.projects)
+    SystemHook.all_hooks_fire({
+      event_name: "key_save",
+      title: key.title,
+      key: key.key,
+      owner_email: key.user.email,
+      owner_name: key.user.name,
+      projects: key.projects,
+      created_at: key.created_at,
+    })
   end
 
   def after_destroy(key)
     return if key.is_deploy_key && !key.last_deploy?
     git_host.remove_key(key.identifier, key.projects)
+    SystemHook.all_hooks_fire({
+      event_name: "key_destroy",
+      title: key.title,
+      key: key.key,
+      owner_email: key.user.email,
+      owner_name: key.user.name,
+      projects: key.projects,
+      created_at: key.created_at,
+    })
   end
 end
